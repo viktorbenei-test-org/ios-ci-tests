6295521590b78cb8d387395cb71d36b66a4fb75f
Viktor Benei <viktor.benei@gmail.com>
2015-04-20 09:41:15 +0200

    base project with .gitignore

---
+++ ContInt/ContInt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContInt
+//
+//  Created by Viktor Benei on 4/20/15.
+//  Copyright (c) 2015 Bitrise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
